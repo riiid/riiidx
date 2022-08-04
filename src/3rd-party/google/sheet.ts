85c679f49677983cb6047466ac7b6ff9c5927175
Yongwook Choi <flow@hrmm.xyz>
2022-08-04 09:36:04 +0900

    Implement app-store-metadata (#2)
    Co-authored-by: JongChan Choi <jong@chan.moe>

--- src/3rd-party/google/sheet.ts
+++ src/3rd-party/google/sheet.ts
@@ -1,3 +1,43 @@
+const sheetsApiUrl = "https://sheets.googleapis.com/v4/spreadsheets/";
+
+export interface Sheet {
+  properties: {
+    sheetId: number;
+    title: string;
+    index: string;
+  };
+}
+
+export interface GetSpreadsheetConfig {
+  apiKey: string;
+  spreadsheetId: string;
+  accessToken: string;
+}
+export interface GetSpreadsheetResult {
+  spreadsheetId: string;
+  properties: {
+    title: string;
+  };
+  sheets: Sheet[];
+}
+export async function getSpreadsheet(
+  config: GetSpreadsheetConfig,
+): Promise<GetSpreadsheetResult> {
+  const { apiKey, spreadsheetId, accessToken } = config;
+  const res = await fetch(
+    `${sheetsApiUrl}${spreadsheetId}/?key=${apiKey}`,
+    {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  if (res.status != 200) throw new Error(await res.text());
+  return await res.json();
+}
+
 export interface GetValueConfig {
   apiKey: string;
   spreadsheetId: string;
@@ -14,7 +54,7 @@ export async function getValues(
 ): Promise<GetValuesResult> {
   const { apiKey, spreadsheetId, range, accessToken } = config;
   const res = await fetch(
-    `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`,
+    `${sheetsApiUrl}${spreadsheetId}/values/${range}?key=${apiKey}`,
     {
       method: "GET",
       headers: {
---
+++ src/team/component/santa/app-store-metadata.ts
@@ -0,0 +1,81 @@
+import { Select } from "https://deno.land/x/cliffy@v0.24.3/prompt/mod.ts";
+import { signIn } from "../../../3rd-party/google/auth.ts";
+import * as fs from "https://deno.land/std@0.150.0/fs/mod.ts";
+import { dirname } from "https://deno.land/std@0.150.0/path/mod.ts";
+import { getSpreadsheet, getValues } from "../../../3rd-party/google/sheet.ts";
+import { getRoot } from "../../../misc/git.ts";
+import { fetchGoogleSecret } from "../../../misc/secret.ts";
+import { createJsonStorage } from "../../../misc/storage.ts";
+
+const spreadsheetId = "1UjczOTryPqDoHD6mGMKoUwMq3__zFVkdN7lu8iWK3lE";
+const scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+
+if (import.meta.main) {
+  const { clientId, clientSecret } = await fetchGoogleSecret();
+  const root = await getRoot();
+  const tokenStorage = await createJsonStorage(
+    root + "/tmp/" + "santa-app-store-google-login.json",
+  );
+  const token = await signIn({ clientId, clientSecret, scopes, tokenStorage });
+  const entries = await getSheets({ accessToken: token.access_token });
+  const title = await Select.prompt({
+    message: "Select a sheet",
+    options: entries.map(([_, name]) => ({
+      name,
+      value: name,
+    })),
+  });
+  const metadata = await getMetadata({
+    accessToken: token.access_token,
+    title,
+  });
+  for (const _path in metadata) {
+    const path = root + "/fastlane/metadata/" + _path;
+    console.error(`Writing ${path}...`);
+    await fs.ensureDir(dirname(path));
+    await Deno.writeTextFile(path, metadata[_path]);
+  }
+  Deno.exit();
+}
+
+export interface GetAllSheetNamesConfig {
+  accessToken: string;
+}
+export type SheetEntry = [number, string];
+export async function getSheets(
+  { accessToken }: GetAllSheetNamesConfig,
+): Promise<SheetEntry[]> {
+  const { apiKey } = await fetchGoogleSecret();
+  const v = await getSpreadsheet({ apiKey, spreadsheetId, accessToken });
+  const entries = v.sheets.map<SheetEntry>(
+    (sheet) => [sheet.properties.sheetId, sheet.properties.title],
+  );
+  return entries;
+}
+
+export interface GetMetadataConfig {
+  accessToken: string;
+  title: string;
+}
+export interface Metadata {
+  [filePath: string]: string;
+}
+export async function getMetadata(
+  { accessToken, title }: GetMetadataConfig,
+): Promise<Metadata> {
+  const { apiKey } = await fetchGoogleSecret();
+  const range = `'${title}'!C3:J3`;
+  const v = await getValues({ apiKey, spreadsheetId, range, accessToken });
+  const [[category, ko, _, ja, vn, tw, us, th]] = v.values;
+  if (category !== "업데이트 안내") {
+    throw new Error("Invalid metadata sheet");
+  }
+  return {
+    "ko/release_note.txt": ko,
+    "ja/release_note.txt": ja,
+    "vi/release_note.txt": vn,
+    "zh-Hant/release_note.txt": tw,
+    "en-US/release_note.txt": us,
+    "th/release_note.txt": th,
+  };
+}
